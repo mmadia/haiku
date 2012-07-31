3ae8e7e548a9b57b2323634638fce06f9f985311
Matt Madia <mattmadia@gmail.com>
2012-07-31 20:54:34 +0000

    Applied Ingo's changes to atomic.cpp from patch on #8730.

--- src/build/libroot/atomic.cpp
+++ src/build/libroot/atomic.cpp
@@ -6,103 +6,104 @@
 #include <SupportDefs.h>
 
 
-int32
-atomic_set(vint32 *value, int32 newValue)
+haiku_build_int32
+_haiku_build_atomic_set(haiku_build_vint32 *value, haiku_build_int32 newValue)
 {
-	int32 oldValue = *value;
+	haiku_build_int32 oldValue = *value;
 	*value = newValue;
 	return oldValue;
 }
 
 
-int32
-atomic_test_and_set(vint32 *value, int32 newValue, int32 testAgainst)
+haiku_build_int32
+_haiku_build_atomic_test_and_set(haiku_build_vint32 *value,
+	haiku_build_int32 newValue, haiku_build_int32 testAgainst)
 {
-	int32 oldValue = *value;
+	haiku_build_int32 oldValue = *value;
 	if (oldValue == testAgainst)
 		*value = newValue;
 	return oldValue;
 }
 
 
-int32
-atomic_add(vint32 *value, int32 addValue)
+haiku_build_int32
+_haiku_build_atomic_add(haiku_build_vint32 *value, haiku_build_int32 addValue)
 {
-	int32 oldValue = *value;
+	haiku_build_int32 oldValue = *value;
 	*value += addValue;
 	return oldValue;
 }
 
 
-int32
-atomic_and(vint32 *value, int32 andValue)
+haiku_build_int32
+_haiku_build_atomic_and(haiku_build_vint32 *value, haiku_build_int32 andValue)
 {
-	int32 oldValue = *value;
+	haiku_build_int32 oldValue = *value;
 	*value &= andValue;
 	return oldValue;
 }
 
 
-int32
-atomic_or(vint32 *value, int32 orValue)
+haiku_build_int32
+_haiku_build_atomic_or(haiku_build_vint32 *value, haiku_build_int32 orValue)
 {
-	int32 oldValue = *value;
+	haiku_build_int32 oldValue = *value;
 	*value |= orValue;
 	return oldValue;
 }
 
 
-int32
-atomic_get(vint32 *value)
+haiku_build_int32
+_haiku_build_atomic_get(haiku_build_vint32 *value)
 {
 	return *value;
 }
 
 
-int64
-atomic_set64(vint64 *value, int64 newValue)
+haiku_build_int64
+_haiku_build_atomic_set64(haiku_build_vint64 *value, haiku_build_int64 newValue)
 {
-	int64 oldValue = *value;
+	haiku_build_int64 oldValue = *value;
 	*value = newValue;
 	return oldValue;
 }
 
-int64
-atomic_test_and_set64(vint64 *value, int64 newValue, int64 testAgainst)
+haiku_build_int64
+_haiku_build_atomic_test_and_set64(haiku_build_vint64 *value,
+	haiku_build_int64 newValue, haiku_build_int64 testAgainst)
 {
-	int64 oldValue = *value;
+	haiku_build_int64 oldValue = *value;
 	if (oldValue == testAgainst)
 		*value = newValue;
 	return oldValue;
 }
 
-int64
-atomic_add64(vint64 *value, int64 addValue)
+haiku_build_int64
+_haiku_build_atomic_add64(haiku_build_vint64 *value, haiku_build_int64 addValue)
 {
-	int64 oldValue = *value;
+	haiku_build_int64 oldValue = *value;
 	*value += addValue;
 	return oldValue;
 }
 
-int64
-atomic_and64(vint64 *value, int64 andValue)
+haiku_build_int64
+_haiku_build_atomic_and64(haiku_build_vint64 *value, haiku_build_int64 andValue)
 {
-	int64 oldValue = *value;
+	haiku_build_int64 oldValue = *value;
 	*value &= andValue;
 	return oldValue;
 }
 
-int64
-atomic_or64(vint64 *value, int64 orValue)
+haiku_build_int64
+_haiku_build_atomic_or64(haiku_build_vint64 *value, haiku_build_int64 orValue)
 {
-	int64 oldValue = *value;
+	haiku_build_int64 oldValue = *value;
 	*value |= orValue;
 	return oldValue;
 }
 
-int64
-atomic_get64(vint64 *value)
+haiku_build_int64
+_haiku_build_atomic_get64(haiku_build_vint64 *value)
 {
 	return *value;
 }
-
