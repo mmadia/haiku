5be46aaea5af9cc5e863a2d7b848e4355e2699da
Matt Madia <mattmadia@gmail.com>
2012-08-05 10:03:17 +0000

    Cleanup. No functional change.

--- src/build/configure/test_limits_header.cpp
+++ src/build/configure/test_limits_header.cpp
@@ -2,6 +2,8 @@
  * Copyright 2012, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
+
+
 #include <limits.h>
 #include <stdio.h>
 
@@ -14,13 +16,6 @@
 int
 main()
 {
-	// NOTE: #8730 -- these are the combined definitions from
-	// /boot/develop/abi/x86/gcc2/tools/gcc-2.95.3-haiku-111122/lib/gcc-lib/\
-	//		i586-pc-haiku/2.95.3-haiku-111122/include/limits.h
-	// /boot/develop/abi/x86/gcc4/tools/gcc-4.6.2-haiku-120121/lib/gcc/\
-	//		i586-pc-haiku/4.6.2/install-tools/include/limits.h
-	// sed -n -e '/\define[^d]/p' limits.h  | sed -e 's/\#//' \
-	//		| awk '{ print "\tPRINT_MACRO\("$2"\);"}' | sort -u > defs.cpp
 	PRINT_MACRO(__INT_MAX__);
 	PRINT_MACRO(__LONG_LONG_MAX__);
 	PRINT_MACRO(__LONG_MAX__);
