febb8ebad7b3f675f465e6ec2638172c19e5b062
Matt Madia <mattmadia@gmail.com>
2012-08-06 10:01:38 +0000

    Add _HAIKU_BUILD_DECLARE_VARIABLE[_ETC]
    _HAIKU_BUILD_DECLARE_CONSTANT was renamed to _HAIKU_BUILD_DECLARE_VARIABLE.

--- headers/build/HaikuBuild.h
+++ headers/build/HaikuBuild.h
@@ -17,17 +17,23 @@
 #ifdef _HAIKU_BUILD_LIBROOT_FUNCTION_WRAPPER
 #	define _HAIKU_BUILD_DECLARE_FUNCTION(returnValue, name, parameters)	\
 		returnValue _haiku_build_##name parameters;
-#	define _HAIKU_BUILD_DECLARE_CONSTANT(type, name) \
+#	define _HAIKU_BUILD_DECLARE_VARIABLE(type, name) \
 		extern type _haiku_build_##name;
+#	define _HAIKU_BUILD_DECLARE_VARIABLE_ETC(type, name, etc) \
+		extern type _haiku_build_##name etc;
 #	define _HAIKU_BUILD_IDENTIFIER(name) _haiku_build_##name
 #else
 #	define _HAIKU_BUILD_DECLARE_FUNCTION(returnValue, name, parameters)	\
 		returnValue _haiku_build_##name parameters;						\
 		returnValue name parameters										\
 			__asm__(_HAIKU_BUILD_ASMNAME("_haiku_build_" #name));
-#	define _HAIKU_BUILD_DECLARE_CONSTANT(type, name) \
+#	define _HAIKU_BUILD_DECLARE_VARIABLE(type, name) \
 		extern type _haiku_build_##name; \
 		extern type name \
+			__asm__(_HAIKU_BUILD_ASMNAME("_haiku_build_" #name));
+#	define _HAIKU_BUILD_DECLARE_VARIABLE_ETC(type, name, etc) \
+		extern type _haiku_build_##name etc; \
+		extern type name[length] \
 			__asm__(_HAIKU_BUILD_ASMNAME("_haiku_build_" #name));
 #	define _HAIKU_BUILD_IDENTIFIER(name) name
 #endif
