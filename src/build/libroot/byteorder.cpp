65b3a25ed428ea905c8fc704483dc6eec09480d2
Matt Madia <mattmadia@gmail.com>
2012-08-06 00:08:15 +0000

    Updated byteorder.cpp.

--- src/build/libroot/byteorder.cpp
+++ src/build/libroot/byteorder.cpp
@@ -5,24 +5,26 @@
 //#	include <BeOSBuildCompatibility.h>
 //#endif
 
-#include <ByteOrder.h>
+#include <target_ByteOrder.h>
 
-uint16
-__swap_int16(uint16 value)
+_haiku_build_uint16
+__haiku_build_swap_int16(_haiku_build_uint16 value)
 {
 	return (value >> 8) | (value << 8);
 }
 
-uint32
-__swap_int32(uint32 value)
+_haiku_build_uint32
+__haiku_build_swap_int32(_haiku_build_uint32 value)
 {
 	return (value >> 24) | ((value & 0xff0000) >> 8) | ((value & 0xff00) << 8)
 		| (value << 24);
 }
 
-uint64
-__swap_int64(uint64 value)
+_haiku_build_uint64
+__haiku_build_swap_int64(_haiku_build_uint64 value)
 {
-	return uint64(__swap_int32(uint32(value >> 32)))
-		| (uint64(__swap_int32(uint32(value))) << 32);
+	return _haiku_build_uint64(
+			__haiku_build_swap_int32(_haiku_build_uint32(value >> 32)))
+		| (_haiku_build_uint64(
+			__haiku_build_swap_int32(_haiku_build_uint32(value))) << 32);
 }
