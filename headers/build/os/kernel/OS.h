7649bcbfe69ef578884b1c196e278fcd4523de20
Matt Madia <mattmadia@gmail.com>
2012-08-10 13:17:49 -0400

    Add empty OS.h

--- headers/build/os/kernel/OS.h
+++ headers/build/os/kernel/OS.h
@@ -1,1 +1,12 @@
-#include <../os/kernel/OS.h>
+/*
+ * Copyright 2002-2012, Haiku Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _HAIKU_BUILD_OS_H
+#define	_HAIKU_BUILD_OS_H
+
+
+#include "os_target/kernel/target_OS.h"
+
+
+#endif
