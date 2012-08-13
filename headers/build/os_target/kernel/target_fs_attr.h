8c5f6ed064999b2856a15446984e14460350ee09
Matt Madia <mattmadia@gmail.com>
2012-08-13 19:26:32 -0400

    Fixed uses of _HAIKU_BUILD_DECLARE_FUNCTION

--- headers/build/os_target/kernel/target_fs_attr.h
+++ headers/build/os_target/kernel/target_fs_attr.h
@@ -26,15 +26,15 @@ _HAIKU_BUILD_DECLARE_FUNCTION(ssize_t, fs_read_attr, (int fd,
 _HAIKU_BUILD_DECLARE_FUNCTION(ssize_t, fs_write_attr, (int fd,
 	const char *attribute, _haiku_build_uint32 type, haiku_build_off_t pos,
 		const void *buffer, size_t readBytes))
-_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_remove_attr(int fd, const char *attribute))
-_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_stat_attr(int fd, const char *attribute,
+_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_remove_attr, (int fd, const char *attribute))
+_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_stat_attr, (int fd, const char *attribute,
 	struct attr_info *attrInfo))
 
-_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_open_attr(const char *path,
+_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_open_attr, (const char *path,
 	const char *attribute, _haiku_build_uint32 type, int openMode))
-_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_fopen_attr(int fd,
+_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_fopen_attr, (int fd,
 	const char *attribute, _haiku_build_uint32 type, int openMode))
-_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_close_attr(int fd))
+_HAIKU_BUILD_DECLARE_FUNCTION(int, fs_close_attr, (int fd))
 
 _HAIKU_BUILD_DECLARE_FUNCTION(DIR*, fs_open_attr_dir, (const char *path))
 _HAIKU_BUILD_DECLARE_FUNCTION(DIR*, fs_lopen_attr_dir, (const char *path))
